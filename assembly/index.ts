aff3939f36bd0b6373be5c81e16a875a6ea88c8e
Connie Leung <cleung334@gmail.com>
2023-12-03 00:29:21 +0800

    feat: add isPrime function

--- assembly/index.ts
+++ assembly/index.ts
@@ -3,3 +3,21 @@
 export function add(a: i32, b: i32): i32 {
   return a + b;
 }
+
+export function isPrime(n: i32): bool {
+  if (n <= 1) {
+    return false;
+  } else if (n === 2 || n === 3) {
+    return false;
+  } else if (n % 2 === 0 || n % 3 === 0) {
+    return false;
+  }
+  
+  for (var i = 5; i <= Math.sqrt(n); i = i + 6) {
+    if (n % i === 0 || n % (i + 2) === 0) {
+      return false;
+    }
+  }
+  
+  return true;
+} 

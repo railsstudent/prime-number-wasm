1e0f89d5389ceab74d3ee2c48cb25b42926e496b
Connie Leung <cleung334@gmail.com>
2023-12-03 15:31:25 +0800

    feat: add findFirstNPrimes

--- assembly/index.ts
+++ assembly/index.ts
@@ -22,42 +22,32 @@ export function isPrime(n: i32): bool {
   return true;
 } 
 
-// function generateprime(N)
-// {
-// 	let primes = []; // Initialize an empty array to store prime numbers
-// 	primes.push(2); // Add 2 as the first prime number
-// 	let num = 3; // Start checking for prime numbers from 3
-// 	while (primes.length < N) { // Keep searching until we
-// 								// find N prime numbers
-// 		let is_prime = true; // Assume the current number is
-// 							// prime until proven otherwise
-// 		for (let i = 0; i < primes.length; i++) {
-// 			if (num % primes[i] == 0) { // If the current number is 
-// 										// divisible by any previously 
-// 										// found prime numbers
+export function findFirstNPrimes(n: i32): Array<i32> {
+  let primes = new Array<i32>(n);
+  for (let i = 0; i < n; i++) {
+    primes[i] = 0;
+  }
+  primes[0] = 2;
+  let num = 3;
 
-// 				is_prime = false; // Then it is not a prime
-// 								// number
-// 				break; // Exit the loop since we've already
-// 					// proven it's not prime
-// 			}
-// 		}
-// 		if (is_prime) { // If the current number is still
-// 						// prime after checking all
-// 						// previously found prime numbers
-// 			primes.push(num); // Add it to our array of
-// 							// prime numbers
-// 		}
-// 		num += 2; // Check the next odd number (since even
-// 				// numbers other than 2 are not prime)
-// 	}
-// 	for (let i = 0; i < primes.length;
-// 		i++) { // Print the first N prime numbers
-// 		console.log(primes[i] + " ");
-// 	}
-// 	console.log("<br>");
-// }
+  let index = 0;
+  while(index < n - 1) {
+    let isPrime = true;
 
-export function getFirstNPrimes(n: i32): Array<i32> {
-  return [1];
+    for (let i = 0; i <= index; i++) {
+      console.log(`${primes[i]}`);
+      if (num % primes[i] === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+
+    if (isPrime) {
+      primes[index + 1] = num;
+      index = index + 1;
+    }
+    num = num + 2;
+  }
+
+  return primes;
 }
